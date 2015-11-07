cd74f858d4d2bd04086346df6b4884ad0ad36aa0
Thomas Schubert <funthomas424242@gmail.com>
2015-11-07 18:02:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install python-pip
 sudo pip install pygithub3
-wget https://github.com/clockfort/GitHub-Backup/blob/master/github-backup.py
+wget https://raw.githubusercontent.com/clockfort/GitHub-Backup/master/github-backup.py
 chmod +x github-backup.py
 mkdir backups
 # github-backup.py funthomas424242 ~/backups
